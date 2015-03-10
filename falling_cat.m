c5399bcf666347c98c802ffd5141a515ef5fcce5
Paul Boehm <boehmp@umich.edu>
2015-03-09 20:23:46 -0400

    In theory, falling cat.m now gives the rate of change of q0 correctly, and q0 finds the trajectory of q0 given trajectories of q1 and q2

--- falling_cat.m
+++ falling_cat.m
@@ -1,3 +1,8 @@
+%returns rate of change of q0 as a function of the configuration and the rate of change of q1 and q2
+function u0 = falling_cat(q0,q1,q2,u1,u2)
+
+
+
 % Falling cat code goes here
 
 % frame g - ground
@@ -27,6 +32,7 @@ R02=[cos(q2),-sin(q2),0;sin(q2),cos(q2),0;0,0,1];
 % position Vectors (All in the c frame!!!)
 % point "a" is at the joint between links 0 and 1, and "b" between
 % links 0 and 2.
+z=[0 0 1]';
 r1a=Rc0 * R01 * [-L1, 0, 0].';
 ra0=Rc0 * [-L0, 0, 0].'; 
 r2b=Rc0 * R02 * [L2, 0, 0].';
@@ -39,9 +45,11 @@ r0c=-rc0;
 r1c=r10+r0c;
 r2c=r20+r0c;
 
+
 % Really long formula for the velocity (or differential change) in u0
 % as a function of u1 and u2. I hope it works!!!  8[
-u0=(u2*((X(r2c) * X(z) * r2b-X(r2c) * ((X(z) * r2b)*M2)/(M2+M1+M0))*M2-(X(r1c) * ((X(z) * r2b)*M2)/(M2+M1+M0))*M1-(X(r0c) * ((X(z) * r2b)*M2)/(M2+M1+M0))*M0+I2)+u1*(-(X(r2c) * ((X(z) * r1a)*M1)/(M2+M1+M0))*M2+(X(r1c) * X(z) * r1a-X(r1c) * ((X(z) * r1a)*M1)/(M2+M1+M0))*M1-(X(r0c) * ((X(z) * r1a)*M1)/(M2+M1+M0))*M0+I1))/((-X(r2c) * ((X(z) * rb0+X(z) * r2b)*M2+(X(z) * ra0+X(z) * r1a)*M1)/(M2+M1+M0)+X(r2c) * X(z) * rb0+X(r2c) * X(z) * r2b)*M2+(-X(r1c) * ((X(z) * rb0+X(z) * r2b)*M2+(X(z) * ra0+X(z) * r1a)*M1)/(M2+M1+M0)+X(r1c) * X(z) * ra0+X(r1c) * X(z) * r1a)*M1-(X(r0c) * ((X(z) * rb0+X(z) * r2b)*M2+(X(z) * ra0+X(z) * r1a)*M1)/(M2+M1+M0))*M0+I2+I1+I0);
+u0=(-u2*((z.' * X(r2c) * X(z) * r2b-z.' * X(r2c) * ((X(z) * r2b)*M2)/(M2+M1+M0))*M2-(z.' * X(r1c) * ((X(z) * r2b)*M2)/(M2+M1+M0))*M1-(z.' * X(r0c) * ((X(z) * r2b)*M2)/(M2+M1+M0))*M0+I2)-u1*(-(z.' * X(r2c) * ((X(z) * r1a)*M1)/(M2+M1+M0))*M2+(z.' * X(r1c) * X(z) * r1a-z.' * X(r1c) * ((X(z) * r1a)*M1)/(M2+M1+M0))*M1-(z.' * X(r0c) * ((X(z) * r1a)*M1)/(M2+M1+M0))*M0+I1))/((-z.' * X(r2c) * ((X(z) * rb0+X(z) * r2b)*M2+(X(z) * ra0+X(z) * r1a)*M1)/(M2+M1+M0)+z.' * X(r2c) * X(z) * rb0+z.' * X(r2c) * X(z) * r2b)*M2+(-z.' * X(r1c) * ((X(z) * rb0+X(z) * r2b)*M2+(X(z) * ra0+X(z) * r1a)*M1)/(M2+M1+M0)+z.' * X(r1c) * X(z) * ra0+z.' * X(r1c) * X(z) * r1a)*M1-(z.' * X(r0c) * ((X(z) * rb0+X(z) * r2b)*M2+(X(z) * ra0+X(z) * r1a)*M1)/(M2+M1+M0))*M0+I2+I1+I0)
 
 
+end
 
--- q0.m
+++ q0.m
@@ -1,7 +1,33 @@
-function q0traj = q0(t,q1,q2)
+% q0(t,q0init,q1traj,q2traj)
+% t         time vector
+% q0init    inital q0 position
+% q1        vector of q1 corresponding to time vector
+% q2        vector of q2 corresponding to time vector
+function q0traj = q0(t,q0init,q1,q2)
 
 
-q0traj=ode45(falling_cat,q1,q2,t)
+%input validation
+if length(q1)!=length(t) || length(q2)!=length(t)
+    error("time and trajectories must all have the same length")
+end
+
+%just using simple approximation rather than trying to see if we can get ode45 working.
+for i=1:length(t)
+    if i==1
+        q0(i)=q0init
+    else
+        if t(i)==t(i-1)
+            error("time cannot stop :)")
+        end
+        dt=t(i)-t(i-1)
+        u1=(q1(i)-q1(i-1))/dt
+        u2=(q2(i)-q2(i-1))/dt
+        u0=falling_cat(q0(i-1),q1(i-1),q2(i-1),u1,u2)
+        q0(i)=q0(i-1)+u0*dt %Euler's method for approximating ODE's?
+    end
+end
+
+
 
 
 end
