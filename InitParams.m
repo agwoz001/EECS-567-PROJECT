9c6f0ea40f762b4078f8d7e7dc5a60eadd67a653
emmatread <emmatread@gmail.com>
2015-03-10 11:26:15 -0400

    Added new files to calc. vectors in proper format for animate. Animation works with other files. To run, first use SignalGenForAnimate, then compile and run the Animate.slx file

---
+++ InitParams.m
@@ -0,0 +1,27 @@
+clear all;
+clc;
+close all;    
+bgain=76;   
+%system parameters
+    
+
+    
+    tend=10.0;
+    ts=1.0*10^(-4);
+    tsc=1.0/1000.0;
+    tslow=1.0/50.0;
+    tsdata=1.0/1000.0;
+  
+bs9=6*bgain;
+bs10=6*bgain;
+bs11=6*bgain;
+w9=0.85*2*pi;
+w10=6.772;
+w11=9.0;
+phi9=0;
+phi10=0;
+phi11=0;
+
+load qvecs
+
+    
---
+++ SignalGenForAnimate.m
@@ -0,0 +1,15 @@
+clear;
+%Enter these variables according to desired falling trajectory
+t=0:.01:10;
+q0init=pi()/2;
+q2=5*t;
+q1=zeros(1,length(t));
+%%%%%%%%%%%%% Use q0 script to calc appropriate q0 angles
+q0=q0(t,q0init,q1,q2);
+
+%%%%%%%%%%%put variables in workspace in appropriate variables
+q1_signal = [t; q1]';
+q2_signal = [t; q2]';
+q0_signal = [t; q0]';
+
+save qvecs q1_signal  q2_signal  q0_signal
