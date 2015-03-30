fdad1c9489ba7bfc5e8c96b4d51c93f3dc5585bb
emmatread <emmatread@gmail.com>
2015-03-30 10:25:54 -0400

    Started optimization script

---
+++ OptimizeLandingAngle.m
@@ -0,0 +1,9 @@
+PhysicalParameters;
+FinalBodyAngle=linspace(0,pi/2);
+u= [1 0 0]'; %point on rigid body, local coord's
+ua= [-2*L1+L0 0 0];
+for i=1:length(FinalBodyAngle
+    R=[cos(FinalBodyAngle(i)),-sin(FinalBodyAngle(i)),0;sin(FinalBodyAngle(i)),cos(FinalBodyAngle(i)),0;0,0,1];
+    c=R*[2*L1+L0 0 0]; %TODO - check: vector to CM of rigid body 
+    x=R*u+c; %global position of point on rigid body
+end
