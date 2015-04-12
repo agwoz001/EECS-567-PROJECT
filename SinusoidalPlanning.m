8f7dea798d4631ca937e7acf4a6d40f75c7eefcb
andrgwoz <andrgwoz@umich.edu>
2015-04-12 18:48:29 -0400

    path planning works. does not animate due to timing

--- SinusoidalPlanning.m
+++ SinusoidalPlanning.m
@@ -1,53 +1,87 @@
+close all;
 clear all;
-deltat = 0.05;
-k = 0.55;
+
+deltat = 0.1;
+phaseangle = pi/2;
+q1_cycle = -pi/2;
+q1_init = 0;
+q2_init = 0;
 q0_init = 45*pi/180;
 q0_end = 75*pi/180;
 
-q1_init = 0;
-q2_init = 0;
-q1_cycle = -pi/2;
-q2_cycle = pi/2 - k;                                                       
+k_test = linspace(0,pi/3,30);
+w_test = 1:10;
+phaseshift = zeros(size(k_test),10);
+
+figure(1)
+hold on
+for i = 1:length(k_test)
+    for w = 1:length(w_test)    
+        phaseshift(i,w) = SinusoidCyclePhaseChange(k_test(i),q0_init,w,phaseangle);        
+    end
+end
+
+%find where phase shift is approximately an integer
+quotient_map = (q0_end-q0_init)./phaseshift;             %find desired phase shift divided by phase of one cycle
+quotient_map(~isfinite(quotient_map)) = 0;               %remove infinities
+integer_map = mod(quotient_map, 1) < 0.1                 %find values that are approximately divide the phase by even cycles
+allowable_phases = integer_map .* phaseshift;
+
+max_phase = max(max(allowable_phases))
+[maxloc_row, maxloc_col] = find(allowable_phases == max(max(allowable_phases)))
+k = k_test(maxloc_row);
+omega = w_test(maxloc_col)
+num_cycles = (q0_end-q0_init)/max_phase
+
+%plot(k_test,phaseshift*180/pi)
+%xlabel('k'); ylabel('phase shift (deg)');
+%plot3(k_test, w_test, phaseshift(:,:))
+
+q2_cycle = pi/2 - k; 
+T = 2*pi/omega;                             %time of 1 oscillation
+
+t = 0:deltat:1.2*num_cycles*T-deltat;       %time of fall
+t1 = t(1:(1/12)*length(t)); 
+t2 = t1(end)+deltat:deltat:num_cycles*T+t1(end);
+t3 = t2(end)+2*deltat:deltat:1.2*num_cycles*T;
+tend = 1.2*num_cycles*T;
 
 %SINUSOIDAL PLANNING
-%robot falls from 490.5m height
-%total fall time = 10 seconds
-t = 0:0.05:10;
-height = 490.5;
-h_t = height - 0.5*9.81.*(t.^2);
 %entiring q cycle for 1 second
-q1_1 = q1_init:(q1_cycle-q1_init)/(19):q1_cycle;
-q2_1 = q2_init:(q2_cycle-q2_init)/(19):q2_cycle;
-q0_1 = q0(t(1:20), q0_init, q1_1, q2_1);
+q1_1 = q1_init:(q1_cycle-q1_init)/(length(t1)-1):q1_cycle;
+q2_1 = q2_init:(q2_cycle-q2_init)/(length(t1)-1):q2_cycle;
+q0_1 = q0(t1, q0_init, q1_1, q2_1);
 
 %entering sinusoidal cycle
-omega = 4;                      %determines frequency of sinusoidal driving
 u1 = k*sin(t);                  %driving signal to inputs
 u2 = -k*cos(t);   
-q1_2 = -k*cos(omega*t)+q1_cycle+k;
-q2_2 = -k*sin(omega*t)+q2_cycle;
+q1_2 = -k/omega*cos(omega*t)+q1_cycle+k/omega;
+q2_2 = -k/omega*sin(omega*t)+q2_cycle;
 q0_2 = q0(t,q0_1(end),q1_2,q2_2);
 
-%desired phase = 75 (desired) - 45 (initial) = 30 degrees
-[M,I] = min(find((q0_2-(45*pi/180)>= 30*pi/180)));
-q1_2 = q1_2(1:M); q2_2 = q2_2(1:M); q0_2 = q0_2(1:M);       %truncate paths
+q1_2 = q1_2(length(t1)+1:length(t1)+length(t2));
+q2_2 = q2_2(length(t1)+1:length(t1)+length(t2));
+q0_2 = q0_2(length(t1)+1:length(t1)+length(t2));
 
 %entering reverse q cycle for 1 second
-q1_3 = q1_2(end):(q1_init-q1_2(end))/(19):q1_init;
-q2_3 = q2_2(end):(q2_init-q2_2(end))/(19):q2_init;
-q0_3 = q0(t(1:20), q0_2(end), q1_3, q2_3);
+q1_3 = q1_2(end):(q1_init-q1_2(end))/(length(t3)-1):q1_init;
+q2_3 = q2_2(end):(q2_init-q2_2(end))/(length(t3)-1):q2_init;
+q0_3 = q0(t3, q0_2(end), q1_3, q2_3);
 
-%find time steps remaining and wait for collision
-q1_4 = q1_3(end)*ones(1,length(t)-length([q1_1,q1_2,q1_3]));
-q2_4 = q2_3(end)*ones(1,length(t)-length([q2_1,q2_2,q2_3]));
-q0_4 = q0_3(end)*ones(1,length(t)-length([q0_1,q0_2,q0_3]));
+% %find time steps remaining and wait for collision
+q0_final = [q0_1, q0_2, q0_3];
+q1_final = [q1_1, q1_2, q1_3];
+q2_final = [q2_1, q2_2, q2_3];
 
-q0_final = [q0_1, q0_2, q0_3, q0_4];
-q1_final = [q1_1, q1_2, q1_3, q1_4];
-q2_final = [q2_1, q2_2, q2_3, q2_4];
+figure(1)
+plot(t,q1_final*180/pi,'r',t,q2_final*180/pi,'b')
+figure(2)
+plot(t,q0_final*180/pi,t, 75*ones(size(q0_final)),'r')
+grid on
 
 q1_signal = [t; q1_final]';
 q2_signal = [t; q2_final]';
 q0_signal = [t; q0_final]';
 save qvecs q1_signal  q2_signal  q0_signal
 
+%final error = 0.6178%
