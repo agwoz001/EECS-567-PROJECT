835da4cfe0c101299605fe0a5fa1d214e835c7af
Paul Boehm <boehmp@umich.edu>
2015-04-14 20:07:16 -0400

    changed plot for optimal landing angle to plot multiple values for n

--- OptimizeLandingAngle.m
+++ OptimizeLandingAngle.m
@@ -7,22 +7,27 @@ PhysicalParameters;
 %     c=R*[2*L1+L0 0 0]; %TODO - check: vector to CM of rigid body 
 %     x=R*u+c; %global position of point on rigid body
 % end
-a=0.157; %m/s^2
-h=220/100; %m
-theta=linspace(10*pi/180,pi/2,100);
-n=.6;  %choose same weight as paper
-vbefore=a*(2*h/a)^.5;
-%vbefore=.8;
-r1=L1;
-r2=r1+L1+L2;
-r3=r2+L2+L3;
-w=(r1*sin(theta)*M1*vbefore+r2*sin(theta)*M2*vbefore+r3*sin(theta)*M3*vbefore)/(I1+M1*r1^2+I2+M2*r2^2+I3+M3*r3^2);
-Tbefore=1/2*(M1+M2+M3)*vbefore^2;
-Tafter=1/2*(I1+M1*r1^2)*w.^2+1/2*(I2+M2*r2^2)*w.^2+1/2*(I3+M3*r3^2)*w.^2;
-troll=1./(w.*tan(theta));
-metric=n*(Tbefore-Tafter)-(1-n)*troll;  %eq. 15
-plot(theta*180/pi,metric);
-%plot(theta,Tbefore-Tafter)
-%figure
-%plot(troll)
-%axis([0,pi/2,0,1000])
+figure(1)
+hold on
+for n=linspace(0,1,10)
+    a=0.157; %m/s^2
+    h=220/100; %m
+    theta=linspace(10*pi/180,pi/2,100);
+    %n=.6;  %choose same weight as paper
+    vbefore=a*(2*h/a)^.5;
+    %vbefore=.8;
+    r1=L1;
+    r2=r1+L1+L2;
+    r3=r2+L2+L3;
+    w=(r1*sin(theta)*M1*vbefore+r2*sin(theta)*M2*vbefore+r3*sin(theta)*M3*vbefore)/(I1+M1*r1^2+I2+M2*r2^2+I3+M3*r3^2);
+    Tbefore=1/2*(M1+M2+M3)*vbefore^2;
+    Tafter=1/2*(I1+M1*r1^2)*w.^2+1/2*(I2+M2*r2^2)*w.^2+1/2*(I3+M3*r3^2)*w.^2;
+    troll=1./(w.*tan(theta));
+    metric=n*(Tbefore-Tafter)-(1-n)*troll;  %eq. 15
+    plot(theta*180/pi,metric);
+    hold on
+    %plot(theta,Tbefore-Tafter)
+    %figure
+    %plot(troll)
+    %axis([0,pi/2,0,1000])
+end
