1eed9b8776233796b64013d5ce3c07bbe04b6d05
emmatread <emmatread@gmail.com>
2015-04-17 22:02:16 -0400

    4 LINKS HELLZ YES

--- SinusoidalPlanning_4link.m
+++ SinusoidalPlanning_4link.m
@@ -2,19 +2,21 @@ close all;
 clear all;
 
 deltat = 0.01;
-phaseangle = pi/2;
-q1_cycle = -pi/2;
-q3_cycle = 0; %??
+phaseangle = pi/3;
+% q1_cycle = -pi/2;
+% q3_cycle = 0;          %??
 q1_init = 0;
 q2_init = 0;
 q3_init = 0;
 q0_init = 45*pi/180;
 q0_end = 75*pi/180;
-precision = 0.1;        %how close to an integer number of periods is acceptable (unitless)
+precision = 0.3;        %how close to an integer number of periods is acceptable (unitless)
 
 
-k_test = linspace(.00001,2.5,250);%linspace(0,pi/3,50);
+%k_test = linspace(.00001,2.5,250);%linspace(0,pi/3,50);
+k_test = linspace(0.0001,2.5,100);
 w_test = linspace(1,5,25);
+w_test = 1;
 phaseshift = zeros(length(k_test),10);
 
 figure(1)
@@ -47,14 +49,15 @@ k = k_test(maxloc_row);
 omega = w_test(maxloc_col)
 num_cycles = (q0_end-q0_init)/max_phase
 
-k = 10*k;
-omega = 10*omega;
+k = 2*k;
+omega = 2*omega;
 
 %plot(k_test,phaseshift*180/pi)
 %xlabel('k'); ylabel('phase shift (deg)');
 %plot3(k_test, w_test, phaseshift(:,:))
-
-q2_cycle = pi/2 - k/omega; 
+q1_cycle = -pi/2+k/omega
+q2_cycle = k/omega*sin(phaseangle)+pi/2 - k/omega; 
+q3_cycle = k/omega*sin(2*phaseangle)+pi/2-k/omega;
 T = 2*pi/omega;                             %time of 1 oscillation
 
 t = 0:deltat:1.2*num_cycles*T-deltat;       %time of fall
@@ -67,39 +70,44 @@ tend = 1.2*num_cycles*T;
 %entiring q cycle for 1 second
 q1_1 = q1_init:(q1_cycle-q1_init)/(length(t1)-1):q1_cycle;
 q2_1 = q2_init:(q2_cycle-q2_init)/(length(t1)-1):q2_cycle;
-q0_1 = q0(t1, q0_init, q1_1, q2_1);
+q3_1 = q3_init:(q3_cycle-q3_init)/(length(t1)-1):q3_cycle;
+%q3_1 = zeros(size(t1));
+q0_1 = q0_4(t1, q0_init, q1_1, q2_1, q3_1);
 
-%entering sinusoidal cycle
-u1 = k*sin(t);                  %driving signal to inputs
-u2 = -k*cos(t);   
-%q1_2 = -k/omega*cos(omega*t)+q1_cycle+k/omega;
-q1_2 = -k/omega*cos(omega*t)+q1_cycle+k/omega;
-q2_2 = -k/omega*sin(omega*t)+q2_cycle;
-q0_2 = q0(t,q0_1(end),q1_2,q2_2);
-% 
-% q1_2 = q1_2(length(t1)+1:length(t1)+length(t2));
-% q2_2 = q2_2(length(t1)+1:length(t1)+length(t2));
-% q0_2 = q0_2(length(t1)+1:length(t1)+length(t2));
+%entering sinusoidal cycle 
+q1_2 = -k/omega*sin(omega*t)-pi/2+k/omega;
+q2_2 = k/omega*sin(omega*t+phaseangle)+pi/2-k/omega;
+q3_2 = k/omega*sin(omega*t+phaseangle*2)+pi/2-k/omega;
+
+q1_2 = -k/omega*sin(omega*t)-pi/2+k/omega;
+q2_2 = k/omega*sin(omega*t+phaseangle)+pi/2-k/omega;
+q3_2 = k/omega*sin(omega*t+phaseangle*2)+pi/2-k/omega;
+
+q0_2 = q0_4(t,q0_1(end),q1_2,q2_2,q3_2);
 
 q1_2 = q1_2(1:length(t2));
 q2_2 = q2_2(1:length(t2));
+q3_2 = q3_2(1:length(t2));
 q0_2 = q0_2(1:length(t2));
 
 
 %entering reverse q cycle for 1 second
 q1_3 = q1_2(end):(q1_init-q1_2(end))/(length(t3)-1):q1_init;
 q2_3 = q2_2(end):(q2_init-q2_2(end))/(length(t3)-1):q2_init;
-q0_3 = q0(t3, q0_2(end), q1_3, q2_3);
+q3_3 = q3_2(end):(q3_init-q3_2(end))/(length(t3)-1):q3_init;
+%q3_3 = zeros(size(t3));
+q0_3 = q0_4(t3, q0_2(end), q1_3, q2_3, q3_3);
 
-% %find time steps remaining and wait for collision
 q0_final = [q0_1, q0_2, q0_3];
 q1_final = [q1_1, q1_2, q1_3];
 q2_final = [q2_1, q2_2, q2_3];
+q3_final = [q3_1, q3_2, q3_3];
+
 
 figure(1)
-plot(t,q1_final*180/pi,'r',t,q2_final*180/pi,'b',...
+plot(t,q1_final*180/pi,'r',t,q2_final*180/pi,'b',t,q3_final*180/pi,'g',...
     t, 90*ones(size(t)), 'k', t, -90*ones(size(t)),'k')
-legend('q1','q2')
+legend('q1','q2','q3')
 title('Path of Joint Angles');
 grid on
 figure(2)
@@ -109,7 +117,8 @@ grid on
 
 q1_signal = [t; q1_final]';
 q2_signal = [t; q2_final]';
+q3_signal = [t; q3_final]';
 q0_signal = [t; q0_final]';
-save qvecs q1_signal  q2_signal  q0_signal
+save qvecs q1_signal  q2_signal  q0_signal q3_signal
 
 %final error = 0.6178%
